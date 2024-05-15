45454477c56d61f1e1f45f991bc043d4bf55c072
Jan Skorupa <245921@edu.p.lodz.pl>
2024-05-15 18:34:57 +0200

    Poprawa widoczności

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,24 +2,24 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int TAB_SIZE = 12;
-    public static final int MAX_INDEX = TAB_SIZE - 1;
-    public static final int DEFAULT_VALUE = -1;
-    public static final int EMPTY_RHYMER_INDEX = -1;
+    private static final int TAB_SIZE = 12;
+    private static final int MAX_INDEX = TAB_SIZE - 1;
+    private static final int DEFAULT_VALUE = -1;
+    private static final int EMPTY_RHYMER_INDEX = -1;
     private final int[] NUMBERS = new int[TAB_SIZE];
 
-    public int total = EMPTY_RHYMER_INDEX;
+    private int total = EMPTY_RHYMER_INDEX;
 
-    public void countIn(int in) {
+    protected void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
     }
 
-    public boolean callCheck() {
+    protected boolean callCheck() {
         return total == DEFAULT_VALUE;
     }
 
-    public boolean isFull() {
+    protected boolean isFull() {
         return total == MAX_INDEX;
     }
 
@@ -29,7 +29,7 @@ public class DefaultCountingOutRhymer {
         return NUMBERS[total];
     }
 
-    public int countOut() {
+    protected int countOut() {
         if (callCheck())
             return DEFAULT_VALUE;
         return NUMBERS[total--];
